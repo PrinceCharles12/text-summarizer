8e44c924440dce2aa05231184085ec4b682a923c
Prince Sharma <124443333+PrinceCharles12@users.noreply.github.com>
2026-03-19 21:14:41 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title("📝 AI Text Summarizer")
+
+st.write("Enter text below to generate summary")
+
+# Load model
+@st.cache_resource
+def load_model():
+    return pipeline("summarization", model="facebook/bart-large-cnn")
+
+summarizer = load_model()
+
+# Input text
+text = st.text_area("Enter text:")
+
+# Button
+if st.button("Summarize"):
+    if text:
+        summary = summarizer(
+            text,
+            max_length=130,
+            min_length=30,
+            do_sample=False
+        )
+        
+        st.subheader("📌 Summary:")
+        st.write(summary[0]['summary_text'])
+    else:
+        st.warning("Please enter some text")
